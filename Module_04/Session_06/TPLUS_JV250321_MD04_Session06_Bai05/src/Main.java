3e739a799f0af5abf7a8db8f81df048e27f73791
TTDung52 <dungtong52@gmail.com>
2025-06-26 23:17:01 +0700

    Homework ss6

--- Module_04/Session_06/TPLUS_JV250321_MD04_Session06_Bai05/src/Main.java
+++ Module_04/Session_06/TPLUS_JV250321_MD04_Session06_Bai05/src/Main.java
@@ -1,5 +1,141 @@
+import java.util.Scanner;
+
 public class Main {
 	public static void main (String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Product[] products = new Product[10];
+		int productCount = 0;
+
+		// MENU
+		do {
+			System.out.println("------ MENU ------");
+			System.out.println("1. Thêm mới sản phẩm");
+			System.out.println("2. Hiển thị danh sách sản phẩm");
+			System.out.println("3. Cập nhật sản phẩm");
+			System.out.println("4. Xóa sản phẩm");
+			System.out.println("5. Thoát");
+			System.out.print("Lựa chọn của bạn: ");
+			int choice = Integer.parseInt(scanner.nextLine());
+
+			switch (choice) {
+				case 1:
+					// Thêm mới sản phẩm
+					// kiểm tra productCount > products.length
+					if (productCount >= products.length) {
+						System.out.println("Danh sách sản phẩm đã đầy. Không thể thêm sản phẩm mới.");
+						break;
+					}
+					System.out.println("--- Thêm sản phẩm mới ---");
+					System.out.print("Nhập ID sản phẩm: ");
+					String id = scanner.nextLine();
+
+					// Kiểm tra ID trùng lặp
+					boolean idDuplicate = false;
+					for (int i = 0; i < productCount; i++) {
+						if (products[i].getId().equals(id)) {
+							idDuplicate = true;
+							break;
+						}
+					}
+					if (idDuplicate) {
+						System.err.println("ID này đã tồn tại");
+					}
+
+					System.out.print("Nhập tên sản phẩm: ");
+					String name = scanner.nextLine();
+
+					System.out.print("Nhập giá sản phẩm: ");
+					double price = Double.parseDouble(scanner.nextLine());  // Cần kiểm tra nhập vào đúng định dạng
+
+					Product newProduct = new Product(id, name, price);
+
+					products[productCount] = newProduct;
+					productCount++;
+					System.out.println("Sản phẩm đã được thêm thành công!");
+					break;
+
+				case 2:
+					// Hiển thị danh sách sản phẩm
+					if (productCount == 0)
+						System.out.println("Chưa có sản phẩm nào trong danh sách");
+					else {
+						System.out.println("--- Danh sách sản phẩm ---");
+						for (int i = 0; i < productCount; i++) {
+							products[i].display();
+							System.out.println("----------");
+						}
+					}
+					break;
+
+				case 3:
+					// Cập nhật sản phẩm
+					if (productCount == 0) {
+						System.out.println("Chưa có sản phẩm nào để cập nhật.");
+						break;
+					}
+
+					System.out.print("Nhập ID sản phẩm cần cập nhật: ");
+					String idUpdate = scanner.nextLine();
+
+					boolean foundForUpdate = false;
+					for (int i = 0; i < productCount; i++) {
+						if (products[i].getId().equals(idUpdate)) {
+							System.out.print("Nhập tên mới: ");
+							String nameUpdate = scanner.nextLine();
+
+							System.out.print("Nhập giá mới: ");
+							double priceUpdate = Double.parseDouble(scanner.nextLine()); // Cần kiểm tra nhập vào
+
+							Product updateProduct = new Product(idUpdate, nameUpdate, priceUpdate);
+							products[i] = updateProduct;
+
+							System.out.println("Sản phẩm cập nhật thành công");
+							foundForUpdate = true;
+							break;
+						}
+					}
+
+					if (!foundForUpdate) {
+						System.err.println("Không tìm thấy sản phẩm có ID '" + idUpdate);
+					}
+					break;
+
+				case 4:
+					// Xóa sản phẩm
+					if (productCount == 0) {
+						System.out.println("Danh sách rỗng. Không có gì để xóa");
+						break;
+					}
+
+					System.out.print("Nhập ID sản phẩm cần xóa:");
+					String idDelete = scanner.nextLine();
+
+					// Tìm chỉ số cho sản phẩm cần xóa
+					int foundIndex = -1;
+					for (int i = 0; i < productCount; i++) {
+						if (products[i].getId().equals(idDelete)) {
+							foundIndex = i;
+							break;
+						}
+					}
+
+					if (foundIndex != -1) {
+						for (int i = foundIndex; i < productCount - 1; i++) {
+							products[i] = products[i + 1];
+						}
+						products[productCount - 1] = null;
+						productCount--;
+						System.out.printf("Xóa sản phẩm có ID %s thành công\n", idDelete);
+					} else {
+						System.err.println("Không tìm thấy sản phẩm có ID '" + idDelete);
+					}
+					break;
+				case 5:
+					System.exit(0);
+				default:
+					System.err.println("Nhập vào số từ 1 đến 5!");
+			}
+		} while (true);
 
 	}
 }
---
+++ Module_04/Session_06/TPLUS_JV250321_MD04_Session06_Bai06/src/Main.java
@@ -0,0 +1,158 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main (String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Category[] categoryList = new Category[10];
+		int categoryCount = 0;
+
+		do {
+			System.out.println("---------- MENU ----------");
+			System.out.println("1. Thêm mới danh mục");
+			System.out.println("2. Hiển thị danh sách danh mục");
+			System.out.println("3. Cập nhật danh mục");
+			System.out.println("4. Xóa danh mục");
+			System.out.println("5. Tìm kiếm danh mục theo tên");
+			System.out.println("6. Thoát");
+			System.out.print("Nhập lựa chọn của bạn: ");
+
+			int choice = Integer.parseInt(scanner.nextLine());
+			switch (choice) {
+				case 1:
+					if (categoryCount >= categoryList.length) {
+						System.out.println("Danh sách danh mục đã đầy. Không thể thêm sản phẩm mới");
+						break;
+					}
+					System.out.printf("Nhập ID danh mục: ");
+					String id = scanner.nextLine();
+
+					// Kiểm tra tồn tại ID này hay chưa
+					boolean isDuplicate = false;
+					for (int i = 0; i < categoryCount; i++) {
+						if (categoryList[i].getId().equals(id)) {
+							isDuplicate = true;
+							break;
+						}
+					}
+					if (isDuplicate) {
+						System.err.println("ID này đã tồn tại!");
+					} else {
+						System.out.printf("Nhập tên danh mục: ");
+						String name = scanner.nextLine();
+						System.out.printf("Nhập mô tả danh mục: ");
+						String description = scanner.nextLine();
+
+						Category newCategory = new Category(id, name, description);
+						categoryList[categoryCount] = newCategory;
+						categoryCount++;
+						System.out.println("Danh mục được thêm thành công");
+					}
+					break;
+
+				case 2:
+					// Hiển thị danh sách danh mục
+					if (categoryCount == 0) {
+						System.out.println("Chưa có danh mục nào trong danh sách.");
+					} else {
+						System.out.println("------ Danh sách danh mục ------");
+						for (int i = 0; i < categoryCount; i++) {
+							categoryList[i].display();
+						}
+					}
+					break;
+
+				case 3:
+					// Cập nhật danh mục
+					if (categoryCount == 0) {
+						System.out.println("Chưa có danh mục nào để cập nhật.");
+						break;
+					}
+
+					System.out.print("Nhập ID cần update: ");
+					String idUpdate = scanner.nextLine();
+
+					// Kiểm tra có tồn tại ID này không
+					boolean isExistForUpdate = false;
+					for (int i = 0; i < categoryCount; i++) {
+						if (categoryList[i].getId().equals(idUpdate)) {
+							System.out.print("Nhập tên cần mới: ");
+							String nameUpdate = scanner.nextLine();
+							System.out.print("Nhập mô tả cần mới: ");
+							String descriptionUpdate = scanner.nextLine();
+
+							Category categoryUpdate = new Category(idUpdate, nameUpdate, descriptionUpdate);
+							categoryList[i] = categoryUpdate;
+							System.out.println("Danh mục cập nhật thành công.");
+
+							isExistForUpdate = true;
+							break;
+						}
+					}
+					if (!isExistForUpdate) {
+						System.err.println("Không tìm thấy danh mục có ID " + idUpdate);
+					}
+					break;
+
+				case 4:
+					// Xóa danh mục
+					if (categoryCount == 0) {
+						System.out.println("Chưa có danh mục nào để xóa.");
+						break;
+					}
+
+					System.out.print("Nhập ID cần xóa: ");
+					String idDelete = scanner.nextLine();
+
+					// Tìm index của ID cần xóa
+					int deleteIndex = -1;
+					for (int i = 0; i < categoryCount; i++) {
+						if (categoryList[i].getId().equals(idDelete)) {
+							deleteIndex = i;
+							break;
+						}
+					}
+
+					if (deleteIndex != -1) {
+						for (int i = deleteIndex; i < categoryCount - 1; i++) {
+							categoryList[i] = categoryList[i + 1];
+						}
+						categoryList[categoryCount - 1] = null;
+						categoryCount--;
+						System.out.println("Xóa danh mục thành công.");
+					} else {
+						System.err.println("Không tìm thấy danh mục có ID " + idDelete);
+					}
+					break;
+
+				case 5:
+					// Tìm kiếm danh mục theo tên
+					if (categoryCount == 0) {
+						System.out.println("Chưa có danh mục nào để tìm kiếm.");
+						break;
+					}
+
+					System.out.print("Nhập tên danh mục cần tìm: ");
+					String nameSearch = scanner.nextLine();
+
+					boolean foundCategory = false;
+					for (int i = 0; i < categoryCount; i++) {
+						if (categoryList[i].getName().toLowerCase().contains(nameSearch.toLowerCase())) {
+							categoryList[i].display();
+							foundCategory = true;
+						}
+					}
+
+					if (!foundCategory) {
+						System.out.println("Không tìm thấy danh mục nào");
+					}
+
+					break;
+				case 6:
+					scanner.close();
+					System.exit(0);
+				default:
+					System.err.println("Nhập các số từ 1-6");
+			}
+		} while (true);
+	}
+}
