e34f6bbb7c23f485e87485d9287e399daac6f6ef
dungtong52 <dungtong52@example.com>
2025-07-10 08:31:56 +0700

    Homework ss14

--- Module_04/Session_14/TPLUS_JV250321_MD04_Session14_Bai02/src/business/BusManager.java
+++ Module_04/Session_14/TPLUS_JV250321_MD04_Session14_Bai02/src/business/BusManager.java
@@ -3,74 +3,112 @@ package business;
 import entity.Bus;
 import validator.Validation;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class BusManager {
-	private final Map <Integer, Bus> busMap = new HashMap <>();
+    private final Map<Integer, Bus> busMap = new HashMap<>();
 
-	public void addBus (Scanner scanner) {
-		Bus bus = new Bus();
-		bus.inputData(scanner);
-		busMap.put(bus.getId(), bus);
-		System.out.println("Thêm xe thành công");
-	}
+    public void addBus(Scanner scanner) {
+        Bus bus = new Bus();
+        bus.inputData(scanner, this);
+        busMap.put(bus.getId(), bus);
+        System.out.println("Thêm xe thành công");
+    }
 
-	public void updateBus (Scanner scanner) {
-	}
+    public void updateBus(Scanner scanner) {
+        System.out.println("Nhập vào mã xe khách muốn cập nhật:");
+        String idInput = scanner.nextLine();
+        if (Validation.isPositiveInteger(idInput)) {
+            int idUpdate = Integer.parseInt(idInput);
+            Optional<Map.Entry<Integer, Bus>> busSearch = busMap.entrySet().stream().filter(item -> item.getKey() == idUpdate).findFirst();
+            busSearch.ifPresentOrElse(busEntry -> {
+                boolean exit = false;
+                do {
+                    System.out.println("1. Cập nhật biển số xe");
+                    System.out.println("2. Cập nhật số ghế ngồi");
+                    System.out.println("3. Cập nhật ngày bắt đầu");
+                    System.out.println("4.Thoát cập nhật");
+                    System.out.print("Lựa chọn của bạn: ");
+                    String choiceInput = scanner.nextLine();
+                    if (Validation.isIntegerInRange(choiceInput, 1, 4)) {
+                        int choice = Integer.parseInt(choiceInput);
+                        Bus bus = busEntry.getValue();
+                        switch (choice) {
+                            case 1 -> bus.setLicensePlate(bus.inputLicensePlate(scanner, this));
+                            case 2 -> bus.setCapacity(bus.inputCapacity(scanner));
+                            case 3 -> bus.setJoiningDate(bus.inputJoiningDate(scanner));
+                            default -> exit = true;
+                        }
+                    } else {
+                        System.err.println("Nhập số nguyên từ 1-4");
+                    }
+                } while (!exit);
+            }, () -> System.err.println("Không tồn tại mã xe khách này!"));
+        } else {
+            System.err.println("Vui lòng nhập mã xe khách hợp lệ!");
+        }
+    }
 
-	public void deleteBus (Scanner scanner) {
-	}
+    public void deleteBus(Scanner scanner) {
+        System.out.println("Nhập vào mã xe khách muốn xóa:");
+        String idInput = scanner.nextLine();
+        if (Validation.isPositiveInteger(idInput)) {
+            int idDelete = Integer.parseInt(idInput);
+            Optional<Map.Entry<Integer, Bus>> busSearch = busMap.entrySet().stream().filter(item -> item.getKey() == idDelete).findFirst();
+            busSearch.ifPresentOrElse(busEntry -> {
+                busMap.entrySet().remove(busEntry);
+            }, () -> System.err.println("Không tồn tại mã xe khách này!"));
+        } else {
+            System.err.println("Vui lòng nhập mã xe khách hợp lệ!");
+        }
+    }
 
-	public void displayBusMap () {
-		if (busMap.isEmpty()) {
-			System.out.println("Danh sách chưa có xe nào");
-		} else {
-			System.out.println("Danh sách xe:");
-			busMap.entrySet().forEach(System.out::println);
-		}
-		int totalCapacity = busMap.values().stream()
-				.mapToInt(item -> item.getCapacity()).sum();
-		System.out.print("Tổng số ghế ngồi tất cả các xe: " + totalCapacity);
-	}
+    public void displayBusMap() {
+        if (busMap.isEmpty()) {
+            System.out.println("Danh sách chưa có xe nào");
+        } else {
+            System.out.println("Danh sách xe:");
+            busMap.entrySet().forEach(System.out::println);
+        }
+        int totalCapacity = busMap.values().stream().mapToInt(Bus::getCapacity).sum();
+        System.out.print("Tổng số ghế ngồi tất cả các xe: " + totalCapacity);
+    }
 
-	public void filterBusByDate (Scanner scanner) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-		System.out.println("Nhập vào ngày muốn tìm:");
-		String dateInput = scanner.nextLine();
-		if (Validation.isValidDate(dateInput, "dd-MM-yyyy")) {
-			LocalDateTime joinDate = LocalDateTime.parse(dateInput, formatter);
-			System.out.printf("Danh sách xe hoạt động ngày %s\n", dateInput);
-			busMap.values().stream()
-					.filter(item -> item.getJoiningDate().toLocalDate().equals(joinDate))
-					.forEach(System.out::println);
-		} else {
-			System.err.println("Nhập sai định dạng!");
-		}
-	}
+    public void filterBusByDate(Scanner scanner) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        System.out.println("Nhập vào ngày muốn tìm:");
+        String dateInput = scanner.nextLine();
+        if (Validation.isValidDate(dateInput, "dd-MM-yyyy")) {
+            LocalDate joinDate = LocalDate.parse(dateInput, formatter);
+            System.out.printf("Danh sách xe hoạt động ngày %s\n", dateInput);
+            busMap.values().stream().filter(item -> item.getJoiningDate().toLocalDate().equals(joinDate)).forEach(System.out::println);
+        } else {
+            System.err.println("Nhập sai định dạng!");
+        }
+    }
 
-	public void findBusByLicensePlate (Scanner scanner) {
-		System.out.println("Nhập vào biển số xe muốn tìm:");
-		String licensePlate = scanner.nextLine();
-		if (!Validation.isEmpty(licensePlate)) {
-			List <Bus> busSearch = busMap.values().stream()
-					.filter(item -> item.getLicensePlate().equals(licensePlate))
-					.toList();
-			if (!busSearch.isEmpty()) {
-				busSearch.forEach(System.out::println);
-			} else {
-				System.out.printf("Không tồn tại biển số xe %s\n", licensePlate);
-			}
-		} else {
-			System.err.println("Không được để trống!");
-		}
-	}
+    public void findBusByLicensePlate(Scanner scanner) {
+        System.out.println("Nhập vào biển số xe muốn tìm:");
+        String licensePlate = scanner.nextLine();
+        if (!Validation.isEmpty(licensePlate)) {
+            List<Bus> busSearch = busMap.values().stream().filter(item -> item.getLicensePlate().equals(licensePlate)).toList();
+            if (!busSearch.isEmpty()) {
+                busSearch.forEach(System.out::println);
+            } else {
+                System.out.printf("Không tồn tại biển số xe %s\n", licensePlate);
+            }
+        } else {
+            System.err.println("Không được để trống!");
+        }
+    }
 
-	public boolean isIdExists (int id) {
-		return busMap.keySet().stream().anyMatch(item -> item == id);
-	}
+    public boolean isIdExists(int id) {
+        return busMap.keySet().stream().anyMatch(item -> item == id);
+    }
+
+    public boolean isPlateExists(String plate) {
+        return busMap.values().stream().anyMatch(item -> item.getLicensePlate().equals(plate));
+    }
 }
--- Module_04/Session_14/TPLUS_JV250321_MD04_Session14_Bai02/src/entity/Bus.java
+++ Module_04/Session_14/TPLUS_JV250321_MD04_Session14_Bai02/src/entity/Bus.java
@@ -1,81 +1,129 @@
 package entity;
 
+import business.BusManager;
+import validator.Validation;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Bus {
-	private int id;
-	private String licensePlate;
-	private int capacity;
-	private LocalDateTime joiningDate;
+    private int id;
+    private String licensePlate;
+    private int capacity;
+    private LocalDateTime joiningDate;
 
-	public Bus () {
-	}
+    public Bus() {
+    }
 
-	public Bus (int id, String licensePlate, int capacity, LocalDateTime joiningDate) {
-		this.id = id;
-		this.licensePlate = licensePlate;
-		this.capacity = capacity;
-		this.joiningDate = joiningDate;
-	}
+    public Bus(int id, String licensePlate, int capacity, LocalDateTime joiningDate) {
+        this.id = id;
+        this.licensePlate = licensePlate;
+        this.capacity = capacity;
+        this.joiningDate = joiningDate;
+    }
 
-	public int getId () {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setId (int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getLicensePlate () {
-		return licensePlate;
-	}
+    public String getLicensePlate() {
+        return licensePlate;
+    }
 
-	public void setLicensePlate (String licensePlate) {
-		this.licensePlate = licensePlate;
-	}
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
 
-	public int getCapacity () {
-		return capacity;
-	}
+    public int getCapacity() {
+        return capacity;
+    }
 
-	public void setCapacity (int capacity) {
-		this.capacity = capacity;
-	}
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 
-	public LocalDateTime getJoiningDate () {
-		return joiningDate;
-	}
+    public LocalDateTime getJoiningDate() {
+        return joiningDate;
+    }
 
-	public void setJoiningDate (LocalDateTime joiningDate) {
-		this.joiningDate = joiningDate;
-	}
+    public void setJoiningDate(LocalDateTime joiningDate) {
+        this.joiningDate = joiningDate;
+    }
 
-	public void inputData (Scanner scanner) {
-		this.id = inputId(scanner);
-		this.licensePlate = inputLicensePlate(scanner);
-		this.capacity = inputCapacity(scanner);
-		this.joiningDate = inputJoiningDate(scanner);
-	}
+    public void inputData(Scanner scanner, BusManager busManager) {
+        this.id = inputId(scanner, busManager);
+        this.licensePlate = inputLicensePlate(scanner, busManager);
+        this.capacity = inputCapacity(scanner);
+        this.joiningDate = inputJoiningDate(scanner);
+    }
 
-	public int inputId (Scanner scanner) {
-	}
+    public int inputId(Scanner scanner, BusManager busManager) {
+        do {
+            System.out.println("Nhập mã xe khách:");
+            String idInput = scanner.nextLine();
+            if (Validation.isPositiveInteger(idInput)) {
+                int id = Integer.parseInt(idInput);
+                if (!busManager.isIdExists(id)) {
+                    return id;
+                } else {
+                    System.err.println("Mã xe đã tồn tại, vui lòng nhập mã khác!");
+                }
+            } else {
+                System.err.println("Vui lòng nhập vào số nguyên dương");
+            }
+        } while (true);
+    }
 
-	public String inputLicensePlate (Scanner scanner) {
-		
-	}
+    public String inputLicensePlate(Scanner scanner, BusManager busManager) {
+        do {
+            System.out.println("Nhập vào biển số xe:");
+            String plate = scanner.nextLine();
+            if (!Validation.isEmpty(plate)) {
+                if (!busManager.isPlateExists(plate)) {
+                    return plate;
+                } else {
+                    System.err.println("Biển số xe đã tồn tại!");
+                }
+            } else {
+                System.err.println("Vui lòng nhập vào biển số xe đúng định dạng!");
+            }
+        } while (true);
+    }
 
-	public int inputCapacity (Scanner scanner) {
-	}
+    public int inputCapacity(Scanner scanner) {
+        do {
+            System.out.println("Nhập vào số ghế ngồi của xe:");
+            String capacity = scanner.nextLine();
+            if (Validation.isPositiveInteger(capacity)) {
+                return Integer.parseInt(capacity);
+            } else {
+                System.err.println("Vui lòng nhập vào đúng định dạng!");
+            }
+        } while (true);
+    }
 
-	public LocalDateTime inputJoiningDate (Scanner scanner) {
-	}
+    public LocalDateTime inputJoiningDate(Scanner scanner) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        do {
+            System.out.println("Nhập vào ngày bắt đầu (dd-MM-yyyy HH:mm):");
+            String joiningDate = scanner.nextLine();
+            if (Validation.isValidDate(joiningDate, "dd-MM-yyyy HH:mm")) {
+                return LocalDateTime.parse(joiningDate, formatter);
+            } else {
+                System.err.println("Vui lòng nhập vào đúng định dạng ngày!");
+            }
+        } while (true);
+    }
 
-	@Override
-	public String toString () {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
-		return String.format("ID: %d - License Plate: %s - Capacity: %d - Joining Date: %s",
-				this.id, this.licensePlate, this.capacity, formatter.format(this.joiningDate));
-	}
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        return String.format("ID: %d - License Plate: %s - Capacity: %d - Joining Date: %s",
+                this.id, this.licensePlate, this.capacity, formatter.format(this.joiningDate));
+    }
 }
